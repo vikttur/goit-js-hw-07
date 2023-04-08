0f7f4ddeb664c0e53f62e175b3aeea1c6b61702d
Viktor Biruk <vikttur.job@gmail.com>
2023-04-09 01:26:59 +0300

    performs task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const listGalleryRef = document.querySelector('.gallery');
+const newImages = creatingNewImages(galleryItems);
+
+listGalleryRef.innerHTML = newImages;
+listGalleryRef.addEventListener('click', onClickImage);
+
+function creatingNewImages(items) {
+	return items.map((item) =>
+		`<li class='gallery__item'>
+			<a class='gallery__link' href='${item.original}'>
+				<img 
+					class='gallery__image'
+					src='${item.preview}'
+					data-source='${item.original}'
+					alt='${item.description}'
+				/>
+			</a>
+		</li>`
+	).join('');
+};
+
+function onClickImage(event) {
+	event.preventDefault();
+
+	if (!event.target.classList.contains('gallery__image')) {
+		return;
+	};
+
+	showModalWindow(event.target.dataset.source);
+};
+
+function showModalWindow(source) {
+	basicLightbox.create(`<img width="1400" height="900" src="${source}">`).show();
+};
